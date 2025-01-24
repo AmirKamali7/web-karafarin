2b5641ae70e278b1e10f35007974949c23d217b4
AmirKamali7 <158176019+AmirKamali7@users.noreply.github.com>
2025-01-25 02:08:25 +0330

    Fix header bug, add article slider, change part of footer

--- script/script.js
+++ script/script.js
@@ -5,3 +5,23 @@ var swiper = new Swiper(".mySwiper", {
     },
     
   });
+  var swiper = new Swiper(".articleSwiper", {
+    slidesPerView: 1, // mobile
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2, // tablet
+            
+        },
+        992: {
+            slidesPerView: 3, // desktop
+        },
+        1200: {
+            slidesPerView: 3, // large
+        },
+    },
+});
